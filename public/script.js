40f36059fb59f31b78748b82846c412255b1b8b1
yineiri9 <yineiri.reyescepeda25@compscihigh.org>
2025-04-08 14:00:14 +0000

    idk

--- public/script.js
+++ public/script.js
@@ -1,7 +1,18 @@
 // Create a function that tests the post route handler [2 pts]
 
+const { json } = require("express/lib/response")
 
+function testPost(){
+    fetch("/create/potion",{
+        method:"POST",
+        headers:{
+
+        },
+      body:JSON.stringify({message:"hello"})
+    })
+}
 
 // Call the function [1 pt]
 // Show us the successful POST fetch [1 pt]
 
+testPost()
